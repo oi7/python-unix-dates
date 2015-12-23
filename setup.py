577b3852761092430b93e968934df485acd43226
Ophir <ophir@itculate.io>
2015-12-23 15:43:42 -0500

    Added packaging for Pypi

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+from os import path
+
+here = path.abspath(path.dirname(__file__))
+
+setup(
+    name='unix_dates',
+    version='0.0.1',
+    description='Python Unix Dates conversion utilities',
+    url='https://github.com/ophirh/python-unix-dates',
+    author='Ophir Horn',
+    license='MIT',
+    keywords=['unix', 'dates', 'utc', 'timestamp'],
+    packages=['unix_dates'],
+)
