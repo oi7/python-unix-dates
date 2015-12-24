1559a7529778c017e5a79f44b49cfa710e2686d1
Ophir <ophir@itculate.io>
2015-12-24 09:45:05 -0500

    Fixed some setup issues

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ here = path.abspath(path.dirname(__file__))
 
 setup(
     name='unix_dates',
-    version='0.1',
+    version='0.1.1',
     description='Python Unix Dates conversion utilities',
     url='https://github.com/ophirh/python-unix-dates',
-    author='Ophir Horn',
-    author_email='ophir@itculate.io',
+    author='Ophir',
+    author_email='opensource@itculate.io',
     license='MIT',
     keywords=['unix', 'dates', 'utc', 'timestamp'],
     packages=['unix_dates'],
